8a98ab7c667e38813160edda0bcd48803ae908b3
Marcus Legendre <marcus.legendre@gmail.com>
2019-07-22 13:32:26 +0200

    Terraform: output hosts.ini for Ansible

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,9 @@ output "ipv4" {
 output "ipv6" {
     value = "${scaleway_server.opennebula-frontend.public_ipv6}"
 }
+
+resource "null_resource" "inventories" {
+  provisioner "local-exec" {
+      command = "echo -e '[all]\n${scaleway_ip.ip.ip}' > ../ansible/hosts.ini"
+  }
+}
